f4c526e9c4dd4abf562f7a6a9da0a0a50b6ad2bf
Javier Paz Ceballos <124625512+Javierpc14@users.noreply.github.com>
2025-09-11 12:18:21 +0200

    Hoja1 Caso 1 y 2

---
+++ Hoja1/Caso1.py
@@ -0,0 +1,15 @@
+# Programa que pida al usuario 2 numeros y se realiza con ellos: suma, resta, division y multiplicacion
+
+num1 = 1
+num2 = 2
+
+print("Introduce el primer numero")
+num1 = int(input())
+
+print("introduce el segundo numero")
+num2 = int(input())
+
+print("la suma de", num1 , "+", num2 , "es =", num1 + num2)
+print("la resta de", num1 , "-", num2 , "es =", num1 - num2)
+print("la multiplicacion de", num1 , "*", num2 , "es =", num1 * num2)
+print("la division de", num1 , "/", num2 , "es =", num1 / num2)
---
+++ Hoja1/Caso2.py
@@ -0,0 +1,18 @@
+# crear un programa que pregunte al usuario su nombre y sus dos apellidos e imprimir por pantalla como se llama
+
+nombre = ""
+ape1 = ""
+ape2 = ""
+
+print("Introduce tu nombre")
+nombre = input()
+
+print("Introduce tu primer apellido")
+ape1 = input()
+
+print("Introduce tu segundo apellido")
+ape2 = input()
+
+print("Te llamas", nombre, ape1, ape2)
+print("Nombre", nombre)
+print("Primer apellido", ape1)
